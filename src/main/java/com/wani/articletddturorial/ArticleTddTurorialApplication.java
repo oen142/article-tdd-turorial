70e6612b23a873055391080e2a6f0d552579b7b4
oen142 <oen142@naver.com>
2021-05-03 23:34:09 +0900

    init

---
+++ src/main/java/com/wani/articletddturorial/ArticleTddTurorialApplication.java
@@ -0,0 +1,13 @@
+package com.wani.articletddturorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleTddTurorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleTddTurorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wani/articletddturorial/ArticleTddTurorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wani.articletddturorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleTddTurorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
